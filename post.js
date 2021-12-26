9f7906e79c67f51fffb566f6b5d6ddef4bdd5cf4
Power Up Studio <76599999+PowerUpStudios@users.noreply.github.com>
2021-12-26 13:35:20 +0100

    Create post.js

---
+++ post.js
@@ -0,0 +1,10 @@
+class Request {
+  constructor(url,method) {
+    this.url = url
+    this.method = method
+    this.response = null
+    fetch(url, {method: method}).then( response => response.json() ).then( response => {
+        this.response = response
+    } );
+  }
+}
